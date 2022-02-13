b23b2cd532b52045bd7a64e1cdcc0570644016fb
Ying Liu <yl1206@georgetown.edu>
2022-02-13 13:54:46 -0500

    assignment

--- sts_pearson.py
+++ sts_pearson.py
@@ -1,8 +1,15 @@
 from scipy.stats import pearsonr
+import jiwer
+import difflib
+from nltk import word_tokenize
+from nltk.translate.nist_score import sentence_nist
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from nltk.metrics.distance import edit_distance
 import argparse
 from util import parse_sts
-
-
+import warnings
+warnings.filterwarnings('ignore')
+    
 def main(sts_data):
     """Calculate pearson correlation between semantic similarity scores and string similarity metrics.
     Data is formatted as in the STS benchmark"""
@@ -15,13 +22,70 @@ def main(sts_data):
     # TODO 2: Calculate each of the the metrics here for each text pair in the dataset
     # HINT: Longest common substring can be complicated. Investigate difflib.SequenceMatcher for a good option.
     score_types = ["NIST", "BLEU", "Word Error Rate", "Longest common substring", "Edit Distance"]
+           
+    nist_scores = []
+    bleu_scores = []
+    wer_scores = []
+    lcs_scores = []
+    ed_scores = []
+    
+    for text_pair in texts:
+        # input tokenized text
+        t1,t2 = text_pair
+        t1_toks = word_tokenize(t1.lower())
+        t2_toks = word_tokenize(t2.lower())
+        
+        # calculate NIST for each text pair
+        try:
+            nist_score = sentence_nist([t1_toks, ], t2_toks)
+        except ZeroDivisionError:
+            nist_score = 0.0
+            
+        nist_scores.append(nist_score)
+        
+        # calculate BLEU for each text pair
+        try:
+            bleu_score = sentence_bleu([t1_toks, ], t2_toks, smoothing_function=SmoothingFunction().method0)
+        except ZeroDivisionError:
+            bleu_score = 0.0
+            
+        bleu_scores.append(bleu_score)
+        
+        # calculate Word Error Rate for each text pair
+        transformation = jiwer.Compose([
+        jiwer.ToLowerCase(),
+        jiwer.RemoveWhiteSpace(replace_by_space=True),
+        jiwer.RemoveMultipleSpaces(),
+        jiwer.ReduceToListOfListOfWords(word_delimiter=" ")
+]) 
+        wer_score = jiwer.wer(t1, t2, truth_transform=transformation, hypothesis_transform=transformation)
+            
+        wer_scores.append(wer_score)
+        
+        # calculate Longest common substring for each text pair        
+        lcs_score = difflib.SequenceMatcher(None, t1, t2).ratio()
+            
+        lcs_scores.append(lcs_score)
+        
+        # calculate Edit Distance for each text pair
+        ed_score = edit_distance(t1, t2)
+        ed_scores.append(ed_score)
+        
+        
+    # create a dictionary to save scores
+    metrics_score = {'NIST': nist_scores,
+                     "BLEU": bleu_scores, 
+                     "Word Error Rate": wer_scores, 
+                     "Longest common substring": lcs_scores, 
+                     "Edit Distance": ed_scores
+                     }
 
     #TODO 3: Calculate pearson r between each metric and the STS labels and report in the README.
     # Sample code to print results. You can alter the printing as you see fit. It is most important to put the results
     # in a table in the README
     print(f"Semantic textual similarity for {sts_data}\n")
     for metric_name in score_types:
-        score = 0.0
+        score = pearsonr(metrics_score[metric_name], labels)[0]
         print(f"{metric_name} correlation: {score:.03f}")
 
     # TODO 4: Complete writeup as specified by TODOs in README (describe metrics; show usage)
